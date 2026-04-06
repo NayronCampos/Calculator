512eba4e6f774d4051e102815328741b0461288f
1567164 <1567164@sga.pucminas.com.br>
2026-04-06 18:10:55 -0300

    finish calculator

--- src/App.js
+++ src/App.js
@@ -15,20 +15,50 @@ export default class App extends Component {
     state = { ...initialState }
 
     addDigit = n => {
-        if (n === '.' && this.state.displayValue.includes('.')) {
+       
+
+        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
+
+         if (n === '.' && !clearDisplay && this.state.displayValue.includes('.')) {
             return
         }
 
-        const clearDisplay = this.state.displayValue === '0' || this.state.clearDisplay
         const currentValue = clearDisplay ? '' : this.state.displayValue
         const displayValue = currentValue + n
         this.setState({ displayValue, clearDisplay: false })
+        
+        if (n !== '.') {
+            const newValue = parseFloat(displayValue)
+            const values = [...this.state.values]
+            values[this.state.current] = newValue
+            this.setState({ values })
+        }
     }
     clearMemory = () => {
         this.setState({ displayValue: '0'})
     }
     setOperation = operation => {
+        if(this.state.current === 0){
+            this.setState({ operation, current: 1, clearDisplay: true })
+        } else {
+            const equals = operation === '='
+            const values = [...this.state.values]
 
+            try{
+            values [0] =
+            eval(`${values[0]} ${this.state.operation} ${values[1]}`)
+        } catch(e){
+            values[0] = this.state.values[0]
+        }
+
+        values[1] = 0 
+        this.setState({
+            displayValue: `${values[0]}`, operation: equals ? null : operation, current: equals ? 0 : 1, clearDisplay: !equals, values,
+        })
+        }
+        
+
+        
     }
 
     render (){
@@ -38,7 +68,7 @@ export default class App extends Component {
         <Display value={this.state.displayValue}/>
        
         <View style = {style.buttons}>
-            <Buttun label = 'AC'triple onClick={this.clearMemory}/>
+            <Buttun label = 'AC' triple onClick={this.clearMemory}/>
             <Buttun label = '/' operation onClick={() => this.setOperation('/')}/>
             <Buttun label = '7' onClick={() => this.addDigit('7')}/>
             <Buttun label = '8' onClick={() => this.addDigit('8')}/>
