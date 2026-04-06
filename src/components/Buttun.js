d659907e86c396c9eee69e43f912ecbf498f91e3
1567164 <1567164@sga.pucminas.com.br>
2026-04-06 17:27:24 -0300

    update no codigo calculadora

--- src/components/Buttun.js
+++ src/components/Buttun.js
@@ -1,35 +1,43 @@
-import React from 'react'
-import { StyleSheet, Text, Dimensions, TouchableHighlight, Button } from 'react-native'
-
-
+import React from 'react';
+import { StyleSheet, Text, Dimensions, TouchableHighlight, View } from 'react-native';
 
 export default props => {
+    const stylesButton = [styles.button]
+    if (props.double) stylesButton.push(styles.buttonDouble)
+    if (props.triple) stylesButton.push(styles.buttonTriple)
+    if (props.operation) stylesButton.push(styles.operationButton)
+
     return (
-
-        <TouchableHighlight onPress = {props.onClick}> 
-            <Text style = {style.button}> {props.label} </Text>
+        <TouchableHighlight onPress={props.onClick}>
+            <View style={stylesButton}>
+                <Text style={styles.buttonText}>{props.label}</Text>
+            </View>
         </TouchableHighlight>
-    )
-}
+    );
+};
 
-const style = StyleSheet.create({
-    button:{
-        fontSize: 40,
-        height: Dimensions.get('window').width/4,
-        padding: 20,
-        backgroundColor: "#000",
-        textAlign: "center",
+const styles = StyleSheet.create({
+    button: {
+        height: Dimensions.get('window').width / 4,
+        width: Dimensions.get('window').width / 4,
+        backgroundColor: "#a28080",
+        justifyContent: 'center',
+        alignItems: 'center',
         borderWidth: 1,
         borderColor: "#888",
     },
-
-})
-
-
-/*
-export default  props => {
-    return(
-        t<TouchableHighlight />
-
-    )
-}*/
+    buttonText: {
+        fontSize: 40,
+        color: 'black', // Ajuste a cor para visibilidade
+    },
+    operationButton:{
+        color: '#fff',
+        backgroundColor: '#fa8231',
+    },
+    buttonDouble:{
+        width: (Dimensions.get('window').width / 4) * 2,
+    },
+    buttonTriple:{
+        width: (Dimensions.get('window').width / 4) * 3,
+    },
+});
